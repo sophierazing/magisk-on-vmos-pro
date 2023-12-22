cabe4fd75641d9e98048c92955a9b12b27cc6757
Enmmmm <enmmmmn@outlook.com>
2023-12-22 16:17:23 +0000

    core-load module trigger
    Signed-off-by: Enmmmm <enmmmmn@outlook.com>

---
+++ load-module/trigger/load-module.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+#清理目录
+rm -rf /sbin/.magisk/mirror/*
+#删除文件
+rm -f /data/adb/continue-magisk
+#加载模块
+/system/bin/sh /data/adb/load-module/load-modules.sh &
+#等待加载
+while [ ! -f "/data/adb/continue-magisk" ]; do sleep 0.0; done
+#删除文件
+rm /data/adb/continue-magisk
+exit 0
